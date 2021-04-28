1534e88af898c858957941fe0292f18708167826
Karna Gowda <karna.gowda@gmail.com>
2021-04-28 10:15:24 -0500

    Adding functions to repository
    These files were stored and edited locally on my Macbook

---
+++ denitfit.py
@@ -0,0 +1,247 @@
+#Karna Gowda 
+#0.1 20210216 Ported MATLAB code to Python
+#0.2 20210420 Removed computation of CIs for rates, which doesn't work for Nar and Nir strains because only one parameter is varied/fit.
+#0.2 20210421 Updated fitYields to fit an intercept as well, and treat Nar/Nir, Nar, and Nir differently. Changed CI to 68% (comparable to 1 standard error)
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+from lmfit import Minimizer, conf_interval
+import statsmodels.api as sm
+import copy
+
+class experiment:
+    #This class contains the experimental data for a given condition
+    def __init__(self, ID, phen, N0, Nend, A0, A, I0, I, t):
+        self.ID = ID
+        self.phen = phen
+        self.N0 = N0
+        self.Nend = Nend
+        self.A0 = A0
+        self.A = A
+        self.I0 = I0
+        self.I = I
+        self.t = t
+        
+def fitYields(experiments):
+    if experiments[0].phen == 'Nar/Nir':
+        DelA = np.array([])
+        DelI = np.array([])
+        DelOD = np.array([])
+        for i in range(0,len(experiments)):
+            DelA = np.append(DelA,experiments[i].A0-experiments[i].A[:,-1])
+            DelI = np.append(DelI,experiments[i].A0-experiments[i].A[:,-1] + experiments[i].I0-experiments[i].I[:,-1])
+            DelOD = np.append(DelOD,experiments[i].Nend-experiments[i].N0)
+        x = np.append(DelA.reshape(-1, 1), DelI.reshape(-1, 1),axis=1)
+        x = np.append(x,np.ones((len(DelOD),1)),axis=1)
+    if experiments[0].phen == 'Nar':
+        DelA = np.array([])
+        DelOD = np.array([])
+        for i in range(0,len(experiments)):
+            DelA = np.append(DelA,experiments[i].A0-experiments[i].A[:,-1])
+            DelOD = np.append(DelOD,experiments[i].Nend-experiments[i].N0)
+        x = np.append(DelA.reshape(-1, 1), np.ones((len(DelOD),1)),axis=1)
+    if experiments[0].phen == 'Nir':
+        DelI = np.array([])
+        DelOD = np.array([])
+        for i in range(0,len(experiments)):
+            DelI = np.append(DelI,experiments[i].I0-experiments[i].I[:,-1])
+            DelOD = np.append(DelOD,experiments[i].Nend-experiments[i].N0)
+        x = np.append(DelI.reshape(-1, 1), np.ones((len(DelOD),1)),axis=1)
+
+    mod = sm.OLS(DelOD, x)
+    res = mod.fit()
+    ci = res.conf_int(0.32)   # 68% confidence interval. comparable to 1 standard error
+    
+    if experiments[0].phen == 'Nar/Nir':
+        gamA = res.params[0]
+        gamI = res.params[1]
+    elif experiments[0].phen == 'Nar':
+        gamA = res.params[0]
+        gamI = 0
+        ci = np.array((ci[0],np.zeros(2),ci[1]))
+    elif experiments[0].phen == 'Nir':
+        gamA = 0
+        gamI = res.params[0]
+        ci = np.array((np.zeros(2),ci[0],ci[1]))
+
+    return gamA, gamI, ci
+        
+def fitRates(params,experiments,n=1):
+    fitter = Minimizer(residualGlobLMFit, params, fcn_args=(experiments,))
+    result_brute = fitter.minimize(method='brute')
+    best_result = copy.deepcopy(result_brute)
+    for candidate in result_brute.candidates:
+        trial = fitter.minimize(method='leastsq', params=candidate.params)
+        if trial.chisqr < best_result.chisqr:
+            best_result = trial
+    #compute 99% CI
+    # ci = conf_interval(fitter, best_result,sigmas=[0.99])
+    # return best_result, ci
+    return best_result
+
+def plotDenitFit(p,experiment,n=1, tick_labels=True, axes_labels=True):
+    lines = np.array([[0.000,0.447,0.741],[0.850,0.325,0.098]])
+    plt.rcParams.update({"text.usetex": False, 'font.size': 16});
+    
+    y0 = np.append(experiment.N0, [np.nanmedian(experiment.A[:,0]), np.nanmedian(experiment.I[:,0])])
+    th = np.linspace(experiment.t[0],experiment.t[-1],256)
+#     y0 = np.append(experiment.N0, [experiment.A0, experiment.I0])
+#     th = np.linspace(0,experiment.t[-1],256)
+    yh = denitODE(y0,th,p,n)
+    
+    plt.plot(th,yh[:,-2],'-',color=(lines[0,0],lines[0,1],lines[0,2]),linewidth=4,alpha=0.5)
+    plt.plot(th,yh[:,-1],'-',color=(lines[1,0],lines[1,1],lines[1,2]),linewidth=4,alpha=0.5)
+    plt.plot(experiment.t,experiment.A.transpose(),'o',color=(lines[0,0],lines[0,1],lines[0,2]))
+    plt.plot(experiment.t,experiment.I.transpose(),'o',color=(lines[1,0],lines[1,1],lines[1,2]))
+    if tick_labels==True:
+        plt.xticks([0,8,16,32,64])
+        plt.yticks([0,0.5,1,1.5,2])
+    else:
+        plt.xticks([])
+        plt.yticks([])
+        
+    if axes_labels==True:
+        plt.xlabel('time (h)')
+        plt.ylabel('NO_2^-, NO_3^- (mM)')
+    
+def convertPTableToMat(params,n=1):
+    if n == 1:
+        p_out = [[params['rA'].value, params['rI'].value, params['kA'].value, params['kI'].value, params['gamA'].value, params['gamI'].value]]
+    else:
+        p_out = np.zeros((n,6))
+        for i in range(0,n):
+            p_out[i,0] = params[i]['rA'].value
+            p_out[i,1] = params[i]['rI'].value
+            p_out[i,2] = params[i]['kA'].value
+            p_out[i,3] = params[i]['kI'].value
+            p_out[i,4] = params[i]['gamA'].value
+            p_out[i,5] = params[i]['gamI'].value
+    return p_out
+
+def residualGlobLMFit(params,experiments,n=1):
+    return residualGlob(convertPTableToMat(params,n),experiments,n)
+
+def residualGlob(p,experiments,n=1):
+    #Computes the residual for all conditions
+    res_out = np.array([])
+    for i in range(0,len(experiments)):
+        res_out = np.append(res_out,residual(p,experiments[i],n))
+    return res_out
+
+def residual(p,experiment,n=1):
+    #Compute the residual vector for the A and I variables using the replicate measurements taken in a given condition
+    y0 = np.append(experiment.N0, [np.nanmedian(experiment.A[:,0]), np.nanmedian(experiment.I[:,0])])
+#     y0 = np.append(experiment.N0, [experiment.A0, experiment.I0])
+    yh = denitODE(y0,experiment.t,p,n)
+    return np.ravel([experiment.A-yh[:,-2],experiment.I-yh[:,-1]])
+
+def denitODE(y0,t,p,n=1):
+    sol = odeint(F, y0, t, Dfun=J, args=(p,n), rtol=1e-6)
+    return sol
+
+def F(y,t,p,n):
+    #ODE RHS
+    #takes p as an ndarray where each row contains the parameters for a given strain
+    Fout = np.zeros(n+2)
+    for i in range(0,n):
+        Fout[ i]  = f(p[i],[y[i],y[-2],y[-1]])
+        Fout[-2] += g(p[i],[y[i],y[-2],y[-1]])
+        Fout[-1] += h(p[i],[y[i],y[-2],y[-1]])
+    return Fout
+
+def J(y,t,p,n):
+    #Jacobian of F
+    #takes p as an ndarray where each row contains the parameters for a given strain
+    Jout = np.zeros((n+2,n+2))
+    for i in range(0,n):
+        Jout[ i, i]  = dfdN(p[i],[y[i],y[-2],y[-1]])
+        Jout[ i,-2]  = dfdA(p[i],[y[i],y[-2],y[-1]])
+        Jout[ i,-1]  = dfdI(p[i],[y[i],y[-2],y[-1]])
+
+        Jout[-2, i]  = dgdN(p[i],[y[i],y[-2],y[-1]])
+        Jout[-2,-2] += dgdA(p[i],[y[i],y[-2],y[-1]])
+
+        Jout[-1, i]  = dhdN(p[i],[y[i],y[-2],y[-1]])
+        Jout[-1,-2] += dhdA(p[i],[y[i],y[-2],y[-1]])
+        Jout[-1,-1] += dhdI(p[i],[y[i],y[-2],y[-1]])
+    return Jout
+
+def f(p,y):
+    #takes p as a 1darray
+    rA     = p[0]
+    rI     = p[1]
+    kA     = p[2]
+    kI     = p[3]
+    gamA   = p[4]
+    gamI   = p[5]
+    return gamA*rA*y[0]*y[1]/(kA + y[1]) + gamI*rI*y[0]*y[2]/(kI + y[2])
+
+def g(p,y):
+    #takes p as a 1darray
+    rA     = p[0]
+    kA     = p[2]
+    return -1*rA*y[0]*y[1]/(kA + y[1])
+
+def h(p,y):
+    #takes p as a 1darray
+    rA     = p[0]
+    rI     = p[1]
+    kA     = p[2]
+    kI     = p[3]
+    return rA*y[0]*y[1]/(kA + y[1]) - rI*y[0]*y[2]/(kI + y[2])
+
+def dfdN(p,y):
+    #takes p as a 1darray
+    rA     = p[0]
+    rI     = p[1]
+    kA     = p[2]
+    kI     = p[3]
+    gamA   = p[4]
+    gamI   = p[5]
+    return gamA*rA*y[1]/(kA + y[1]) + gamI*rI*y[2]/(kI + y[2])
+
+def dfdA(p,y):
+    #takes p as a 1darray
+    rA     = p[0]
+    kA     = p[2]
+    gamA   = p[4]
+    return gamA*rA*kA*y[0]/(kA + y[1])**2
+
+def dfdI(p,y):
+    #takes p as a 1darray
+    rI     = p[1]
+    kI     = p[3]
+    gamI   = p[5]
+    return gamI*rI*kI*y[0]/(kI + y[2])**2
+
+def dgdN(p,y):
+    #takes p as a 1darray
+    rA     = p[0]
+    kA     = p[2]
+    return -1*rA*y[1]/(kA + y[1])
+
+def dgdA(p,y):
+    #takes p as a 1darray
+    rA     = p[0]
+    kA     = p[2]
+    return -1*rA*kA*y[0]/(kA + y[1])**2
+
+def dhdN(p,y):
+    #takes p as a 1darray
+    rA     = p[0]
+    rI     = p[1]
+    kA     = p[2]
+    kI     = p[3]
+    return rA*y[1]/(kA + y[1]) - rI*y[2]/(kI + y[2])
+
+def dhdA(p,y):
+    #takes p as a 1darray
+    rA     = p[0]
+    kA     = p[2]
+    return rA*kA*y[0]/(kA + y[1])**2
+
+def dhdI(p,y):
+    #takes p as a 1darray
+    rI     = p[1]
+    kI     = p[3]
+    return -1*rI*kI*y[0]/(kI + y[2])**2
---
+++ griess.py
@@ -0,0 +1,123 @@
+#Karna Gowda
+#0.0.1
+
+import bmgdata as bd
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+def find_outliers(data):
+    #returns true for elements more than 1.5 interquartile ranges above the upper quartile.
+    
+    q75 = np.quantile(data,0.75)
+    q25 = np.quantile(data,0.25)
+    is_outlier = data > q75 + (q75-q25)*1.5
+    return is_outlier
+
+def remove_bubbles(data_in,data_540,data_900):
+    #find wells with bubbles in the no2no3 measurements
+    
+    bub_in  = find_outliers(data_in['900'])
+    bub_900 = find_outliers(data_900)
+
+    #replace values at 540 nm
+    data_out = data_in.copy()
+    for idx in bub_in.index[bub_in == True].tolist():
+        replacement = data_540.loc[idx]
+        data_out['540'].loc[idx] = np.median(replacement[~bub_900.loc[idx]]) #replace with median of values that don't have bubbles
+    return data_out
+
+def read_griess(meta_fn,data_fn=None,data_540_fn=None,data_900_fn=None):
+    #returns absorbance data at 540 nm
+    #corrects for bubbles if well scan measurements are provided
+    #there are three valid cases that this function works for:
+    #1) A file name is provided for data_fn. Usually this is for importing no2 data.
+    #2) File names are provided for data_fn and associated well scan files data_540_fn and data_900_fn. Usually this is for importing old no2no3 measurements using both endpoint and well scan data.
+    #3) File names are provided for only well scan files data_540_fn and data_900_fn. This is the prefered measurement data for future experiments.
+    
+    meta       = pd.read_csv(meta_fn,index_col=0).dropna(how='all')  #import metadata
+    
+    if (data_540_fn != None) & (data_900_fn != None): 
+            data_540 = bd.read_abs_wellscan(data_540_fn) #import well scan data (540 nm)
+            data_540 = data_540[data_540.index.isin(meta.index)] #remove indices with no values in the metadata
+            data_900 = bd.read_abs_wellscan(data_900_fn) #import well scan data (900 nm)
+            data_900 = data_900[data_900.index.isin(meta.index)] #remove indices with no values in the metadata
+            
+    if data_fn != None: #case 1 or 2
+        data_out    = bd.read_abs_endpoint(data_fn) #import data
+        data_out    = data_out[data_out.index.isin(meta.index)] #remove indices with no values in the metadata
+        #correct for bubbles
+        if data_540_fn != None: #case 2
+            data_out = remove_bubbles(data_out,data_540,data_900) #correct for bubbles in measurement.
+        data_out = data_out['540'] #pick out 540 nm measurement
+    elif (data_540_fn != None) & (data_900_fn != None): #case 3
+        bub_900 = find_outliers(data_900)
+        data_540[bub_900] = np.NaN #Set bubbles to NaN
+        data_out = data_540.median(axis=1)
+        data_out = data_out.rename("540")
+        
+    return data_out
+
+def fit_griess(meta_fn,no2_fn,no2no3_fn=None,no2no3_540_fn=None,no2no3_900_fn=None):
+    meta = pd.read_csv(meta_fn,index_col=0).dropna(how='all')
+    meta = meta[['NO2','NO3']] #keep only the NO2/NO3 concentration columns
+    no2 = read_griess(meta_fn,no2_fn)
+    no2no3 = read_griess(meta_fn,data_fn = no2no3_fn,data_540_fn=no2no3_540_fn,data_900_fn=no2no3_900_fn)
+    
+    #remove nan values
+    nan_idx = meta['NO2'].index[meta['NO2'].apply(np.isnan)].union(meta['NO3'].index[meta['NO3'].apply(np.isnan)])
+    meta = meta.drop(nan_idx)
+    no2 = no2.drop(nan_idx)
+    no2no3 = no2no3.drop(nan_idx)
+    
+    #subtract blank values
+    blank_idx = meta.index[(meta['NO2']==0) & (meta['NO3']==0)].tolist()
+    no2_blank = no2.loc[blank_idx].median()
+    no2no3_blank = no2no3.loc[blank_idx].median()
+    no2 = no2 - no2_blank
+    no2no3 = no2no3 - no2no3_blank
+
+    #identify pure no2 and pure no3 samples
+    no2_std_idx = meta.index[(meta['NO2']>0) & (meta['NO3']==0)].tolist()
+    no3_std_idx = meta.index[(meta['NO2']==0) & (meta['NO3']>0)].tolist()
+
+    #no2 standard curve, griess measurement
+    y = no2.loc[no2_std_idx].values.reshape(-1, 1)
+    x = meta['NO2'].loc[no2_std_idx].values.reshape(-1, 1)
+
+    #Fit a quadratic model to the nitrite concentration
+    x = np.append(x, x**2,axis=1) #make x and x^2 the independent variables
+    reg = LinearRegression().fit(x, y)
+    g_fit = [reg.intercept_[0], reg.coef_[0][0], reg.coef_[0][1]]
+
+    #no2 and no3 standard curves, vcl3 measurement
+    y1 = no2no3.loc[no2_std_idx].values
+    y2 = no2no3.loc[no3_std_idx].values
+    x1 = meta.loc[no2_std_idx].values
+    x2 = meta.loc[no3_std_idx].values
+
+    #Fit a quadratic model to the sum of nitrate and nitrite concentrations
+    x = np.append(np.sum(x1,axis=1),np.sum(x2,axis=1)).reshape(-1,1)
+    x = np.append(x, x**2,axis=1) #make x and x^2 the independent variables
+    y = np.append(y1,y2).reshape(-1,1)
+    reg = LinearRegression().fit(x, y)
+    v_fit = [reg.intercept_[0], reg.coef_[0][0], reg.coef_[0][1]]
+
+    fit = [[no2_blank,no2no3_blank], g_fit, v_fit]
+    
+    return fit
+
+def invert_griess(no2,fit,no2no3=None):
+    #Returns inferred concentrations
+    NO2 = ((-fit[1][1] + np.sqrt(fit[1][1]**2 - 4*(fit[1][0]-no2)*fit[1][2]))/2/fit[1][2]).rename("NO2"); #solve quadratic formula
+    NO2[NO2<0] = 0.0
+    if isinstance(no2no3,pd.Series):
+        NO3 = (((-fit[2][1] + np.sqrt(fit[2][1]**2 - 4*(fit[2][0]-no2no3)*fit[2][2]))/2/fit[2][2]) - NO2).rename("NO3"); #solve quadratic formula
+        NO3[NO3<0] = 0.0
+    else:
+        NO3 = NO2.copy().rename("NO3")
+        NO3[NO3!=0] = 0.0
+
+    data_out = pd.DataFrame()
+    data_out = data_out.append([NO2,NO3]).transpose()
+    return data_out
